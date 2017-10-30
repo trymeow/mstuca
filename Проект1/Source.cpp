19e31bf61682b68ed8a2144abb10492d8e4cf6a9
trymeow <uarewritingme@gmail.com>
2017-10-30 22:48:17 +0300

    функция parse

--- Проект1/Source.cpp
+++ Проект1/Source.cpp
@@ -11,54 +11,62 @@ class Programmer {
 public:
 	void setName(string name) {
 		name_ = name;
-	};
+	}
 	string getName() {
 		return name_;
-	};
+	}
 	void setIBMnumber(unsigned number) {
 		IBMnumber_ = number;
-	};
+	}
 	unsigned getIBMnumber() {
 		return IBMnumber_;
-	};
+	}
 	void setcipher(int n) {
 		cipher_ = n;
-	};
+	}
 	int getcipher() {
 		return cipher_;
-	};
+	}
 	void setdate(string date) {
 		date_ = date;
-	};
+	}
 	string getdate () {
 		return date_;
-	
-	};
-	void setStime(unsigned time) {
-		stime_ = time;
-	};
-	void setEtime(unsigned time) {
-		etime_ = time;
-	};
+	}
+	void setStime(string str) {
+		istringstream buffer(str);
+		unsigned hour;
+		buffer >> hour;
+		stime_ = hour * 60 ;
+		buffer.ignore();
+		buffer >> hour;
+		stime_ += hour ;
+	}
+	void setEtime(string str) {
+		istringstream buffer(str);
+		unsigned hour;
+		buffer >> hour;
+		etime_ = hour * 60;
+		buffer.ignore();
+		buffer >> hour;
+		etime_ += hour;
+	}
 	unsigned getStime() {
 		return stime_;
-	};
+	}
 	unsigned getEtime() {
 		return etime_;
 	}
 	void appendToName(string name) {
 		name_.append(name);
 	}
-	void buf(string str) {
+	void setIBMnumber(string str) {
 		istringstream buffer(str);
 		buffer >> IBMnumber_;
-		buffer.clear();
-		;
 	}
-	void bufci(string str) {
+	void setcipher(string str) {
 		istringstream buffer(str);
 		buffer >> cipher_;
-		buffer.clear();
 	}
 
 private:
@@ -72,45 +80,36 @@ private:
 
 };
 
-int main() {
-	//comment
-	setlocale(LC_ALL, "rus");
+
+Programmer parse( string filename) {
 	ifstream fin;
-	fin.open("Text.txt");
+	fin.open(filename);
 	Programmer a;
 	string str;
 	fin >> str;
-	a.setName (str);
+	a.setName(str);
 	a.appendToName(" ");
 	fin >> str;
 	a.appendToName(str);
 	fin >> str;
-	a.buf(str);
+	a.setIBMnumber(str);
 	fin >> str;
-	a.bufci(str);
+	a.setcipher(str);
 	fin >> str;
 	a.setdate(str);
 	fin >> str;
-	istringstream buffer(str);
-	buffer.str(str);
-	unsigned hour;
-	buffer >> hour;
-	a.setStime(hour * 60);
-	buffer.ignore();
-	buffer >> hour;
-	a.setStime(hour + a.getStime()) ;
+	a.setStime(str);
 	fin >> str;
-	buffer.clear();
-	buffer.str(str);
-	buffer >> hour;
-	a.setEtime(hour * 60);
-	buffer.ignore();
-	buffer >> hour ;
-	a.setEtime(hour + a.getEtime()) ;
+	a.setEtime(str);
+	return a ;
+}
 
-
-	
-	cout << a.getName()<< " "<< a.getIBMnumber()<< " " << a.getcipher() << " " << a.getdate()<< " " << a.getStime()<< " "<< a.getEtime();
+int main() {
+	//comment
+	setlocale(LC_ALL, "rus");
+	ifstream fin;
+	Programmer vovan = parse("Text.txt");
+	cout << vovan.getName()<< " "<< vovan.getIBMnumber()<< " " << vovan.getcipher() << " " << vovan.getdate()<< " " << vovan.getStime()<< " "<< vovan.getEtime();
 	system("pause");
 	return 0;
 }
