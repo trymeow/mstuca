0e23ad497b5a66920bdaa4d17e5ca058820110a5
trymeow <uarewritingme@gmail.com>
2017-11-12 16:31:40 +0300

    запись строк в бинарный

--- Проект1/Source.cpp
+++ Проект1/Source.cpp
@@ -69,7 +69,7 @@ public:
 		istringstream buffer(str);
 		buffer >> cipher_;
 	}
-
+	
 private:
 	string name_;
 	unsigned IBMnumber_;
@@ -114,11 +114,50 @@ deque<Programmer> parse( string filename) {
 int main() {
 	//comment
 	setlocale(LC_ALL, "rus");
+	ofstream fout;
 	ifstream fin;
+
 	deque<Programmer> a = parse("Text.txt");
 	while (!a.empty()) {
 		Programmer c = a.front();   
 		cout << c.getName() << " " << c.getIBMnumber() << " " << c.getcipher() << " " << c.getdate() << " " << c.getStime() << " " << c.getEtime() << endl;
+		string IBMnumber = to_string(c.getIBMnumber());
+		IBMnumber.append(".dat");
+		cout << "IBMnumber: " << IBMnumber << endl;
+		fout.open(IBMnumber, ios::out | ios::binary);
+		if (!fout) { cout << "Ошибка открытия бинарного файла\n"; system("pause"); exit(0); }
+		string  strn = c.getName();
+		int n = strn.length();
+		fout.write((char*)&n, sizeof(n));
+		const char* str1 = strn.c_str();
+		fout.write((char*)&str1, sizeof(str1));
+		int tmp = c.getcipher();
+		fout.write((char*)&tmp, sizeof(tmp));
+		string  strd = c.getdate();
+		int d = strd.length();
+		fout.write((char*)&n, sizeof(n));
+		const char* str2 = strd.c_str();
+		fout.write((char*)&str2, sizeof(str2));
+		tmp = c.getStime();
+		fout.write((char*)&tmp, sizeof(tmp));
+		tmp = c.getEtime();
+		fout.write((char*)&tmp, sizeof(tmp));
+		fout.close();
+		fin.open(IBMnumber, ios::in | ios::binary);
+		fin.read((char*)&n, sizeof(n));
+		cout << n << endl;
+		fin.read((char*)&str1, sizeof(str1));
+		cout << str1 << endl; 
+		fin.read((char*)&tmp, sizeof(tmp));
+		cout << tmp << endl  ;
+		fin.read((char*)&d, sizeof(d));
+		fin.read((char*)&str2, sizeof(str2));
+		cout << str2 << endl;
+		fin.read((char*)&tmp, sizeof(tmp));
+		cout << tmp << endl;
+		fin.read((char*)&tmp, sizeof(tmp));
+		cout << tmp << endl;
+		fin.close();
 		a.pop_front();
 	};
 	system("pause");
